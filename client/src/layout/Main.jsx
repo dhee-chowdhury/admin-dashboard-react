e01a584e8cc6cbfc12b8c1df30909500b828d850
dhee-chowdhury <dhee.chowdhury@gmail.com>
2023-11-22 18:53:45 +0600

    add sidebar

--- client/src/layout/Main.jsx
+++ client/src/layout/Main.jsx
@@ -3,13 +3,24 @@ import { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Main = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
-    <Box width="100%" height="100%">
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box>
-        {/* Navbar here */}
-        <Navbar />
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
